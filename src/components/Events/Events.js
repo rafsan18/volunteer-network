b6b2b8909b28bb8e59176f87570e472bf4cd4491
rafsan <rafsanamin18@gmail.com>
2020-10-05 11:54:15 +0600

    load data from server

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Events = () => {
+const Events = (props) => {
+    console.log(props);
     return (
         <div>
             <h1>This is event page</h1>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Events from "../Events/Events";
 import Headers from "../Headers/Headers";
 
 const Home = () => {
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/events")
+            .then((res) => res.json())
+            .then((data) => setEvents(data));
+    }, []);
+
     return (
         <div>
             <Headers></Headers>
-            <h1>This is home component</h1>
+            {events.map((event) => (
+                <Events key={event._id} event={event}></Events>
+            ))}
         </div>
     );
 };
